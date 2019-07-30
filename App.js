60dab4ab02b43f95c73756c8ccf7adb497be486b
Norrismi <Norrismi0@gmail.com>
2019-07-29 22:35:20 -0400

    first

---
+++ App.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+
+import Header from "./src/components/header";
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Header />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 75,
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10
+  },
+  instructions: {
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
+});
