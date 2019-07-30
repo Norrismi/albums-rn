b0dca26710110b29026056325fcc7a1ab47dff1b
Norrismi <Norrismi0@gmail.com>
2019-07-30 15:37:36 -0400

    starting styles

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,27 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 const Header = () => {
   return (
-    <View>
-      <Text>Albums!</Text>
+    <View style={styles.viewStyle}>
+      <Text style={styles.textStyle}>Albums!</Text>
     </View>
   );
 };
 
 export default Header;
+
+const styles = StyleSheet.create({
+  viewStyle: {
+    height: 50,
+    width: "95%",
+    borderColor: 'black',
+    borderWidth: 1,
+    justifyContent: 'center'
+  },
+
+  textStyle: {
+    color: "red",
+    textAlign: 'center',
+  }
+});
